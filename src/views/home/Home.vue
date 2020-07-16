ed6e6785eb53197dff8480e9eeba48b743e7d99d
Jeffrey-Niu <1210708168@qq.com>
2020-07-16 20:01:27 +0800

    完善首页

---
+++ src/views/home/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
+import HomeSwiper from "./childrenComps/HomeSwiper";
+import RecommendView from "./childrenComps/RecommendView";
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    };
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  }
+};
+</script>
+
+<style scoped>
+.home-nav {
+  color: #fff;
+  background-color: var(--color-tint);
+}
+</style>
